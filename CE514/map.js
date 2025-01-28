369536ab7c8d5a0bfa6d38213a5def5764f7029f
imathews-238 <156374416+imathews-238@users.noreply.github.com>
2025-01-28 07:49:19 -0700

    Update map.js

--- CE514/map.js
+++ CE514/map.js
@@ -84,7 +84,7 @@ document.getElementById('distance-form').addEventListener('submit', function (e)
     const dLat = lat2 - lat1;
     const dLon = lon2 - lon1;
 
-    const a = Math.sin(dLat / 2) ** 2 + Math.cos(lat1) * Math.cos(lat2) + Math.sin(dLon / 2) ** 2;
+    const a = Math.sin(dLat / 2) ** 2 + Math.cos(lat1) * Math.cos(lat2) * Math.sin(dLon / 2) ** 2;
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     const distance = R * c;
 
