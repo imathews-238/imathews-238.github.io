3e4ac8f43c921352008eb24afbc2110d31eb3cb4
imathews-238 <156374416+imathews-238@users.noreply.github.com>
2025-01-29 15:55:29 -0700

    Update kmlmap.js

--- CE514/kmlmap.js
+++ CE514/kmlmap.js
@@ -1,47 +1,30 @@
-const map = L.map('map');
+const map = L.map('map').setView([38.85, -104.85], 10); // Set initial view
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-fetch('https://imathews-238.github.io/CE514/UC_cell_towers.kml') // Ensure this KML file is accessible
-    .then(res => res.text())
-    .then(kmltext => {
-        var parser = new DOMParser();
-        var kml = parser.parseFromString(kmltext, 'text/xml');
-        var track = new L.KML(kml);
-        map.addLayer(track);
-        map.fitBounds(track.getBounds());
-    });
+function loadKML(url) {
+    fetch(url)
+        .then(res => {
+            if (!res.ok) throw new Error(`Failed to load ${url}`);
+            return res.text();
+        })
+        .then(kmltext => {
+            var parser = new DOMParser();
+            var kml = parser.parseFromString(kmltext, 'text/xml');
+            var track = new L.KML(kml);
+            map.addLayer(track);
+            console.log(`Loaded ${url}`);
+        })
+        .catch(error => console.error(error));
+}
 
-fetch('https://imathews-238.github.io/CE514/UC_major_roads.kml') // Ensure this KML file is accessible
-    .then(res => res.text())
-    .then(kmltext => {
-        var parser = new DOMParser();
-        var kml = parser.parseFromString(kmltext, 'text/xml');
-        var track = new L.KML(kml);
-        map.addLayer(track);
-        map.fitBounds(track.getBounds());
-    });
+loadKML('https://imathews-238.github.io/CE514/UC_cell_towers.kml');
+loadKML('https://imathews-238.github.io/CE514/UC_major_roads.kml');
+loadKML('https://imathews-238.github.io/CE514/Utah_county_boundary.kml');
 
-fetch('https://imathews-238.github.io/CE514/Utah_county_boundary.kml') // Ensure this KML file is accessible
-    .then(res => res.text())
-    .then(kmltext => {
-        var parser = new DOMParser();
-        var kml = parser.parseFromString(kmltext, 'text/xml');
-        var track = new L.KML(kml);
-        map.addLayer(track);
-        map.fitBounds(track.getBounds());
-    });
-
-const locations = [
-    { name: "My House", coords: [38.9011, -104.77125] },
-    { name: "William J. Palmer High School", coords: [38.839422463309546, -104.82074570592077] },
-    { name: "Church of Jesus Christ of Latter-day Saints", coords: [38.87115396280131, -104.79815034137457] },
-    { name: "Garden of the Gods", coords: [38.8783, -104.8691] },
-    { name: "Pikes Peak", coords: [38.8409, -105.0423] }
-];
 
 locations.forEach(location => {
-    L.marker(location.coords).addTo(map).bindPopup(location.name);
+    L.marker(location.coords).addTo(map).bindPopup("Cell Phone Tower");
 });
