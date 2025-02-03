5bed37f4c4b6937d0a08794aaa0700685ef89fc4
imathews-238 <156374416+imathews-238@users.noreply.github.com>
2025-02-03 09:57:52 -0700

    Update kmlmap.js

--- CE514/kmlmap.js
+++ CE514/kmlmap.js
@@ -73,3 +73,58 @@ omnivore.kml('https://imathews-238.github.io/CE514/UC_cell_towers.kml')
         }
     })
     .addTo(map);
+
+var LocationLayers = {};  // Object to store country layers
+
+function init() {
+    // Initialize the map and set the global view
+    map = L.map("map_id").setView([20, 0], 2);
+
+    // Add OpenStreetMap tiles
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // Load KML files into countryLayers object
+    let Location = ["australia", "bahrain", "canada", "cayman", "czechia", "england", 
+                     "france", "germany", "japan", "mexico", "spain", "switzerland", 
+                     "thailand", "thenetherlands", "usa", "vietnam"];
+    
+    countries.forEach(Location => {
+        LocationLayers[country] = omnivore.kml(`kml/${Location}.kml`).on('ready', function() {
+            this.setStyle({color: "blue"});  // Optional: Set country boundary color
+        });
+    });
+
+    // Reset view to the world
+    setLocation("none");
+}
+
+function setLocation(theLocation) {
+    console.log("Selected:", theLocation);
+
+    // Remove all layers before adding a new one
+    Object.values(LocationLayers).forEach(layer => map.removeLayer(layer));
+
+    if (theCountry !== "none") {
+        let selectedLayer = LocationLayers[theLocation];
+        if (selectedLayer) {
+            map.addLayer(selectedLayer);
+            map.fitBounds(selectedLayer.getBounds());
+            document.getElementById("viewing").innerText = fullName(theLocation);
+        }
+    } else {
+        map.setView([20, 0], 2);
+        document.getElementById("viewing").innerText = "Earth";
+    }
+}
+
+function fullName(country) {
+    const names = {
+        "england": "United Kingdom",
+        "usa": "United States of America",
+        "thenetherlands": "The Netherlands",
+        "cayman": "Cayman Islands"
+    };
+    return names[country] || country.charAt(0).toUpperCase() + country.slice(1);
+}
